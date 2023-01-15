4c9ac6356a2e7e89596d9705a91017ec0b858a5f
student <student@it16-003.it.local>
2023-01-15 14:08:46 +0100

    v1.4

--- Projekt.cpp
+++ Projekt.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-    double a, b;
-    cout << "Podaj a: ";
+    double a, b, c;
+    cout << "Podaj A: ";
     cin >> a;
-    cout << "Podaj b: ";
+    cout << "Podaj B: ";
     cin >> b;
+    cout << "Podaj C: ";
+    cin >> c;
 
- if (a == 0) {
-        if(b == 0) 
-            cout << "Funkcja posiada nieskoñczenie wiele miejsc 0.";
+    if (a == 0 && b == 0) {
+        if(c == 0) 
+            cout << "Funkcja jest to¿sama z 0, ma nieskoñczenie wiele miejsc 0.";
         else
-            cout << "Funkcja nie posiada miejsc 0.";
+            cout << "Funkcja jest liniowo zale¿na, nie ma miejsc 0.";
+    }
+    else if (a == 0 && b != 0) {
+        double x = -c/b;
+        cout << "Miejsce zerowe funkcji to: (0, " << x << ")";
+    }
+    else if (b == 0 && a != 0) {
+        double x = -c/a;
+        cout << "Miejsce zerowe funkcji to: (" << x << ", 0)";
     }
     else {
-        double x = -b/a;
-        cout << "Miejsce 0 funkcji to: " << x;
+        double delta = b*b - 4*a*c;
+        if(delta < 0) 
+            cout << "Funkcja nie ma rozwi¹zañ rzeczywistych.";
+        else if(delta == 0){
+            double x = -b/(2*a);
+            cout << "Funkcja ma jedno miejsce 0: (" << x << ", 0)";
+        }
+        else{
+            double x1 = (-b + sqrt(delta))/(2*a);
+            double x2 = (-b - sqrt(delta))/(2*a);
+            cout << "Funkcja ma dwa miejsca 0: (" << x1 << ", 0) oraz (" << x2 << ", 0)";
+        }
     }
 
     return 0;
-}
