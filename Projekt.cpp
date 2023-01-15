8e9d5100633fb54477a49b365051794ab95c650e
student <student@it16-003.it.local>
2023-01-15 14:15:51 +0100

    v2.0

--- Projekt.cpp
+++ Projekt.cpp
@@ -11,19 +11,9 @@ int main() {
     cout << "Podaj C: ";
     cin >> c;
 
-    if (a == 0 && b == 0) {
-        if(c == 0) 
-            cout << "Funkcja jest to¿sama z 0, ma nieskoñczenie wiele miejsc 0.";
-        else
-            cout << "Funkcja jest liniowo zale¿na, nie ma miejsc 0.";
-    }
-    else if (a == 0 && b != 0) {
-        double x = -c/b;
-        cout << "Miejsce zerowe funkcji to: (0, " << x << ")";
-    }
-    else if (b == 0 && a != 0) {
-        double x = -c/a;
-        cout << "Miejsce zerowe funkcji to: (" << x << ", 0)";
+   
+    if (a == 0) {
+        cout << "To nie jest funkcja kwadratowa!";
     }
     else {
         double delta = b*b - 4*a*c;
@@ -31,13 +21,15 @@ int main() {
             cout << "Funkcja nie ma rozwi¹zañ rzeczywistych.";
         else if(delta == 0){
             double x = -b/(2*a);
-            cout << "Funkcja ma jedno miejsce 0: (" << x << ", 0)";
+            cout << "Funkcja ma jedno miejsce 0: " << x;
         }
         else{
             double x1 = (-b + sqrt(delta))/(2*a);
             double x2 = (-b - sqrt(delta))/(2*a);
-            cout << "Funkcja ma dwa miejsca 0: (" << x1 << ", 0) oraz (" << x2 << ", 0)";
+
+            cout << "Funkcja ma dwa miejsca 0: " << x1 << " oraz " << x2;
         }
     }
 
     return 0;
+}
